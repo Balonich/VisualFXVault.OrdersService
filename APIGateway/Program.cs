c864a083e7d106537ad5e6353cbfc4501328150d
Nikita Larushkin <balonich@gmail.com>
2025-07-06 19:12:17 +0200

    Add QoS and Rate Limiting options to Ocelot configuration; update Program.cs to include Polly and CacheManager services

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,9 +1,17 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services
+    .AddOcelot(builder.Configuration)
+    .AddPolly()
+    .AddCacheManager(config =>
+    {
+        config.WithDictionaryHandle();
+    });
 
 var app = builder.Build();
 await app.UseOcelot();
